e82f6a1d25b23cd1c8814f64e2b83e86b534695f
Renaud Jenny <renaud.jenny@gmail.com>
2020-04-20 21:48:49 +0100

    fix(Villagers): Swipe gesture dismiss the keyboard
    * Previously you had to tap "return" if you want to dismiss the
    keyboard

---
+++ ACHNBrowserUI/ACHNBrowserUI/viewModifiers/DismissingKeyboardOnSwipe.swift
@@ -0,0 +1,31 @@
+//
+//  DismissingKeyboardOnSwipe.swift
+//  ACHNBrowserUI
+//
+//  Created by Renaud JENNY on 20/04/2020.
+//  Copyright © 2020 Thomas Ricouard. All rights reserved.
+//
+
+import SwiftUI
+
+// See https://stackoverflow.com/questions/56491386/how-to-hide-keyboard-when-using-swiftui
+struct DismissingKeyboardOnSwipe: ViewModifier {
+    func body(content: Content) -> some View {
+        content.gesture(swipeGesture)
+    }
+
+    private var swipeGesture: some Gesture {
+        DragGesture(minimumDistance: 10, coordinateSpace: .global)
+            .onChanged(endEditing)
+    }
+
+    private func endEditing(_ gesture: DragGesture.Value) {
+        UIApplication.shared.connectedScenes
+            .filter {$0.activationState == .foregroundActive}
+            .map {$0 as? UIWindowScene}
+            .compactMap({$0})
+            .first?.windows
+            .filter {$0.isKeyWindow}
+            .first?.endEditing(true)
+    }
+}
