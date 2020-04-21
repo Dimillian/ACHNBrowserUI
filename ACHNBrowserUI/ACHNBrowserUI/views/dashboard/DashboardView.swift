77e96f2a94d89a3fe807520fb769da698c0cc5c6
Thomas Ricouard <ricouard77@gmail.com>
2020-04-21 08:35:03 +0200

    Revert "fix: hide villager birthday section if there aren't any"
    This reverts commit 17fddd94619c70b09621594b88d950a4121114d1.

--- ACHNBrowserUI/ACHNBrowserUI/views/dashboard/DashboardView.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/dashboard/DashboardView.swift
@@ -209,16 +209,12 @@ extension DashboardView {
     }
     
     func makeBirthdayView() -> some View {
-        Group {
-            if !villagersViewModel.todayBirthdays.isEmpty {
-                Section(header: makeSectionHeader(icon: "icon-present", text: "Villager Birthday's")) {
-                    ForEach(villagersViewModel.todayBirthdays) { villager in
-                        NavigationLink(destination: VillagerDetailView(villager: villager),
-                                       label: {
-                                        VillagerRowView(villager: villager)
-                        })
-                    }
-                }
+        Section(header: makeSectionHeader(icon: "icon-present", text: "Villager birthday")) {
+            ForEach(villagersViewModel.todayBirthdays) { villager in
+                NavigationLink(destination: VillagerDetailView(villager: villager),
+                               label: {
+                                VillagerRowView(villager: villager)
+                })
             }
         }
     }
