42ded004f7d7444bfeb2d3532dd9211b109e2f70
Thomas Ricouard <ricouard77@gmail.com>
2020-04-21 17:40:09 +0200

    Add events and next events on the dashboard

--- ACHNBrowserUI/ACHNBrowserUI/views/dashboard/EventTextView.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/dashboard/EventTextView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 import ACNHEvents
 
 struct EventTextView: View {
-    let today = Date()
+    let todayEvents = Date().events(for: .north)
+    let nextEvent = Event.nextEvent()
+    
+    func makeDateBadge(date: Date) -> some View {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM"
+        return Text(formatter.string(from: date))
+            .foregroundColor(.white)
+            .font(.caption)
+            .fontWeight(.bold)
+            .padding(8)
+            .background(Color.grass)
+            .cornerRadius(20)
+    }
     
     var body: some View {
         Group {
-            if today.isEarthDay {
-                Text("Today is Earth Day!")
+            if !todayEvents.isEmpty {
+                Text("Today is \(todayEvents.first!.title()) ")
+            } else if nextEvent.1 != nil {
+                HStack {
+                    Text("Next event: \(nextEvent.1!.title())!")
+                    Spacer()
+                    makeDateBadge(date: nextEvent.0)
+                }
             } else {
                 Text("No events today.")
             }
