3b3383aefcad5da9e5d6c73a45470c4a2d92088c
Matt Bonney <bonneym@gmail.com>
2020-04-30 13:01:26 -0400

    Tweaked Navbar font + colors, tweaked table view colors

--- ACHNBrowserUI/ACHNBrowserUI/SceneDelegate.swift
+++ ACHNBrowserUI/ACHNBrowserUI/SceneDelegate.swift
@@ -22,18 +22,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         if let windowScene = scene as? UIWindowScene {
             
             //TODO: Move that to SwiftUI once implemented
+            let descriptor = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .title1).withSymbolicTraits(.traitBold)?.withDesign(UIFontDescriptor.SystemDesign.rounded)
+            let descriptor2 = UIFontDescriptor.preferredFontDescriptor(withTextStyle: .largeTitle).withSymbolicTraits(.traitBold)?.withDesign(UIFontDescriptor.SystemDesign.rounded)
+            
             UINavigationBar.appearance().largeTitleTextAttributes = [
-                NSAttributedString.Key.foregroundColor: UIColor.white]
+                NSAttributedString.Key.font:UIFont.init(descriptor: descriptor2!, size: 34),
+                NSAttributedString.Key.foregroundColor: UIColor(named: "text")!
+            ]
             
             UINavigationBar.appearance().titleTextAttributes = [
-                NSAttributedString.Key.foregroundColor: UIColor.white]
+                NSAttributedString.Key.font:UIFont.init(descriptor: descriptor!, size: 17),
+                NSAttributedString.Key.foregroundColor: UIColor(named: "text")!
+            ]
             
-            UINavigationBar.appearance().barTintColor = UIColor(named: "grass")
-            UINavigationBar.appearance().backgroundColor = UIColor(named: "grass")
-            UINavigationBar.appearance().tintColor = .white
-            UINavigationBar.appearance().prefersLargeTitles = false
+            UINavigationBar.appearance().barTintColor = UIColor(named: "dialogue")
+            UINavigationBar.appearance().backgroundColor = UIColor(named: "dialogue-reverse")
+            UINavigationBar.appearance().tintColor = UIColor(named: "text")
             
-            UITableView.appearance().backgroundColor = UIColor(named: "dialogue")
+            UITableView.appearance().backgroundColor = UIColor(named: "dialogue-reverse")
             UITableViewCell.appearance().backgroundColor = UIColor(named: "dialogue")
             UITableView.appearance().tableFooterView = UIView()
             
