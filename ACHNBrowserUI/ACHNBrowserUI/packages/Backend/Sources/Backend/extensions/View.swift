6b75b6d7a676238a08edfc7b67e5944bcdaf20da
Thomas Ricouard <ricouard77@gmail.com>
2020-05-03 08:35:32 +0200

    Share items + lock rotation on iPhone

---
+++ ACHNBrowserUI/ACHNBrowserUI/packages/Backend/Sources/Backend/extensions/View.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Thomas Ricouard on 03/05/2020.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    public func asImage() -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        
+        controller.view.frame = CGRect(x: 0, y: CGFloat(Int.max), width: 1, height: 1)
+        UIApplication.shared.windows.first!.rootViewController?.view.addSubview(controller.view)
+        
+        let size = controller.sizeThatFits(in: UIScreen.main.bounds.size)
+        controller.view.bounds = CGRect(origin: .zero, size: size)
+        controller.view.sizeToFit()
+        
+        let image = controller.view.asImage()
+        controller.view.removeFromSuperview()
+        return image
+    }
+}
+
+extension UIView {
+    public func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            rendererContext.cgContext.addPath(
+            UIBezierPath(roundedRect: bounds, cornerRadius: 20).cgPath)
+            rendererContext.cgContext.clip()
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+}
---
+++ ACHNBrowserUI/ACHNBrowserUI/views/representable/ActivityControllerView.swift
@@ -0,0 +1,31 @@
+//
+//  ActivityController.swift
+//  ACHNBrowserUI
+//
+//  Created by Thomas Ricouard on 03/05/2020.
+//  Copyright © 2020 Thomas Ricouard. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+public struct ActivityControllerView: UIViewControllerRepresentable {
+    
+    public let activityItems: [Any]
+    public let applicationActivities: [UIActivity]?
+    
+    public init(activityItems: [Any], applicationActivities: [UIActivity]?) {
+        self.activityItems = activityItems
+        self.applicationActivities = applicationActivities
+    }
+    
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityControllerView>) -> UIActivityViewController {
+        return UIActivityViewController(activityItems: activityItems,
+                                        applicationActivities: applicationActivities)
+    }
+    
+    public func updateUIViewController(_ uiViewController: UIActivityViewController,
+                                       context: UIViewControllerRepresentableContext<ActivityControllerView>) {
+        
+    }
+}
