41821e7cdfcdd8857fbf590d35b6789a7a66388f
Renaud Jenny <renaud.jenny@gmail.com>
2020-05-06 22:04:39 +0100

    feat(Turnips): flat the curve ;) for Chart
    * Fix #77. Use a nice algo to have a nice Bézier curve instead of
    straight lines for Turnips Chart

--- ACHNBrowserUI/ACHNBrowserUI/views/turnips/charts/TurnipsChartMinMaxCurves.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/turnips/charts/TurnipsChartMinMaxCurves.swift
@@ -29,7 +29,7 @@ struct TurnipsChartMinMaxCurves: Shape {
                 let y = ratioY * (maxY - CGFloat(minValue))
                 return CGPoint(x: x, y: y)
             })
-        path.addLines(minPoints)
+        path.addHermiteCurvedLines(points: minPoints)
 
         let maxPoints: [CGPoint] = minMax
             .compactMap { $0.second }
@@ -41,10 +41,57 @@ struct TurnipsChartMinMaxCurves: Shape {
             })
             .reversed()
         path.addLine(to: maxPoints.first ?? .zero)
-        path.addLines(maxPoints)
-
+        path.addHermiteCurvedLines(points: maxPoints)
         path.addLine(to: minPoints.first ?? .zero)
 
         return path
+    }
+}
+
+private extension CGPoint {
+    static func +(lhs: Self, rhs: Self) -> Self {
+        Self(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+
+    static func -(lhs: Self, rhs: Self) -> Self {
+        Self(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+
+    static func /(lhs: Self, rhs: CGFloat) -> Self {
+        Self(x: lhs.x/rhs, y: lhs.y/rhs)
+    }
+
+    static func *(lhs: Self, rhs: CGFloat) -> Self {
+        Self(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+}
+
+// Inspired from  https://github.com/FlexMonkey/SmoothScribble/blob/master/SmoothScribble/extensions/UIBezierPathExtension.swift
+extension Path {
+    mutating func addHermiteCurvedLines(points : [CGPoint], alpha : CGFloat = 1/3) {
+        guard !points.isEmpty else { return }
+        move(to: points.first ?? .zero)
+
+        for (i, currentPoint) in points.enumerated() {
+            if currentPoint == points.last { continue }
+
+            let previousPoint = points[max(0, i - 1)]
+            let nextPoint = points[min(i + 1, points.count - 1)]
+
+            let previousDerivative = (nextPoint - previousPoint)/2
+            let control1 = currentPoint + previousDerivative * alpha
+
+            let furtherPoint = points[min(i + 2, points.count - 1)]
+
+            let nextDerivative: CGPoint
+            if i < points.count - 1 {
+                nextDerivative = (furtherPoint - currentPoint)/2
+            } else {
+                nextDerivative = (nextPoint - currentPoint)/2
+            }
+            let control2 = nextPoint - nextDerivative * alpha
+
+            self.addCurve(to: nextPoint, control1: control1, control2: control2)
+        }
     }
 }
