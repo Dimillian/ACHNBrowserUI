3daa083d7dce7a0507beef38c4d2be5719dfbae3
Matt Bonney <bonneym@gmail.com>
2020-05-08 15:50:37 -0400

    Began pulling in logic

--- ACHNBrowserUI/ACHNBrowserUI/views/todayDashboard/TodayCurrentlyAvailableSection.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/todayDashboard/TodayCurrentlyAvailableSection.swift
@@ -7,20 +7,67 @@
 //
 
 import SwiftUI
+import Backend
 
 struct TodayCurrentlyAvailableSection: View {
+    @EnvironmentObject private var collection: UserCollection
+    @ObservedObject var viewModel: DashboardViewModel
+    
     private enum CellType: String {
         case fish = "Fish"
         case bugs = "Ins"
     }
     
+    // MARK: - Bug Calculations
+    private var bugsCaught: Int {
+        if !viewModel.bugs.isEmpty {
+            return collection.caughtIn(list: viewModel.bugs)
+        } else {
+            return 1
+        }
+    }
+    
+    private var bugsAvailable: Int {
+        if !viewModel.bugs.isEmpty {
+            return viewModel.bugs.filterActive().count
+        } else {
+            return 1
+        }
+    }
+    
+    private var newBugs: Int {
+        viewModel.bugs.filterActive().filter{ $0.isNewThisMonth() }.count
+    }
+    
+    // MARK: - Fish Calculations
+    private var fishCaught: Int {
+        if !viewModel.fishes.isEmpty {
+            return collection.caughtIn(list: viewModel.fishes)
+        } else {
+            return 1
+        }
+    }
+    
+    private var fishAvailable: Int {
+        if !viewModel.fishes.isEmpty {
+            return viewModel.fishes.filterActive().count
+        } else {
+            return 1
+        }
+    }
+    
+    private var newFish: Int {
+        viewModel.fishes.filterActive().filter{ $0.isNewThisMonth() }.count
+    }
+    
+    // MARK: - Body
     var body: some View {
         Section(header: SectionHeaderView(text: "Currently Available", icon: "calendar")) {
             NavigationLink(destination: NavigationView { List { Text("Event 2 Detail View") }}) {
                 HStack(alignment: .top) {
-                    makeCell(for: .fish, available: 34, numberNew: 8)
+                    makeCell(for: .fish, caught: fishCaught, available: fishAvailable, numberNew: newFish)
                     Divider()
-                    makeCell(for: .bugs, available: 34, numberNew: 0)
+                    makeCell(for: .bugs, caught: bugsCaught, available: bugsAvailable, numberNew: newBugs)
                 }
             }
             .frame(maxWidth: .infinity)
@@ -28,7 +75,7 @@ struct TodayCurrentlyAvailableSection: View {
         }
     }
     
-    private func makeCell(for type: CellType, available: Int, numberNew: Int = 0) -> some View {
+    private func makeCell(for type: CellType, caught: Int, available: Int, numberNew: Int = 0) -> some View {
         VStack(spacing: 0) {
             Image("\(type.rawValue)\(self.dayNumber())")
                 .resizable()
@@ -37,7 +84,7 @@ struct TodayCurrentlyAvailableSection: View {
             
             Group {
                 // @TODO: Rename all the insect images from "Ins00" to "Bugs00"
-                type == .bugs ? Text("\(available) Bugs") : Text("\(available) \(type.rawValue)")
+                type == .bugs ? Text("\(caught)/\(available) Bugs") : Text("\(caught)/\(available) Fish")
             }
             .font(.system(.headline, design: .rounded))
             .foregroundColor(Color("ACText"))
@@ -65,7 +112,7 @@ struct TodayCurrentlyAvailableSection_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             List {
-                TodayCurrentlyAvailableSection()
+                TodayCurrentlyAvailableSection(viewModel: DashboardViewModel())
             }
             .listStyle(GroupedListStyle())
             .environment(\.horizontalSizeClass, .regular)
