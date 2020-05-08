3bbc69126344bcc51d921cba240531bf3ccf2425
Peter Steinberger <steipete@gmail.com>
2020-05-08 16:46:20 +0200

    Add first simple UI test that iterates through all tab bars

---
+++ ACHNBrowserUI/ACHNBrowserUI-UITests/ACHNBrowserUI_UITests.swift
@@ -0,0 +1,38 @@
+//
+//  ACHNBrowserUI_UITests.swift
+//  ACHNBrowserUI-UITests
+//
+//  Created by Peter Steinberger on 08.05.20.
+//  Copyright © 2020 Thomas Ricouard. All rights reserved.
+//
+
+import XCTest
+
+class ACHNBrowserUI_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testTapAllTabBarButtons() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Simply iterate over all tabs
+        let tabBarsQuery = XCUIApplication().tabBars
+        tabBarsQuery.buttons["Catalog"].tap()
+        tabBarsQuery.buttons["Turnips"].tap()
+        tabBarsQuery.buttons["Villagers"].tap()
+        tabBarsQuery.buttons["My Stuff"].tap()
+        tabBarsQuery.buttons["Dashboard"].tap()
+
+        // Select bugs from the table
+        app.tables/*@START_MENU_TOKEN@*/.buttons["Bugs"]/*[[".cells",".segmentedControls.buttons[\"Bugs\"]",".buttons[\"Bugs\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+    }
+
+}
