17c5e1a140c9b6bfd3977ba7c3bb932176cb9af2
Jan van Heesch <Jan.vanHeesch@gmail.com>
2020-05-09 15:42:53 +0200

    Translated ActiveCrittersView to German

--- ACHNBrowserUI/ACHNBrowserUI/views/critters/ActiveCrittersView.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/critters/ActiveCrittersView.swift
@@ -79,7 +79,7 @@ struct ActiveCrittersView: View {
         List {
             Picker(selection: $selectedTab, label: Text("")) {
                 ForEach(Tab.allCases, id: \.self) { tab in
-                    Text(tab.rawValue).tag(tab.rawValue)
+                    Text(LocalizedStringKey(tab.rawValue)).tag(tab.rawValue)
                 }
             }
             .pickerStyle(SegmentedPickerStyle())
