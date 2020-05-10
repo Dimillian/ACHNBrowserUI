6d627f069ff9c0b65aa9cebff25c529157998eb8
Jan <Jan.vanHeesch@gmail.com>
2020-05-10 22:02:11 +0200

    Small bug fixes at SubscriptionView (#103)
    * Changes due to code review
    
    * Fixed Terms of Use:
    - fixed typo: changed 'term' to 'terms'
    - fixed bug: Terms of Use button on SubscriptionView was not localized
    
    * Make Close button great again

--- ACHNBrowserUI/ACHNBrowserUI/views/about/AboutView.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/about/AboutView.swift
@@ -84,7 +84,7 @@ struct AboutView: View {
                     makeRow(image: "lock", text: "Privacy Policy", color: .acHeaderBackground).onTapGesture {
                         self.selectedSheet = .safari(URL(string: "https://github.com/Dimillian/ACHNBrowserUI/blob/master/privacy-policy.md#ac-helper-privacy-policy")!)
                     }
-                    makeRow(image: "person", text: "Term of Use", color: .acHeaderBackground).onTapGesture {
+                    makeRow(image: "person", text: "Terms of Use", color: .acHeaderBackground).onTapGesture {
                         self.selectedSheet = .safari(URL(string: "https://github.com/Dimillian/ACHNBrowserUI/blob/master/term-of-use.md#ac-helper-term-of-use")!)
                     }
                     makeDetailRow(image: "tag",
