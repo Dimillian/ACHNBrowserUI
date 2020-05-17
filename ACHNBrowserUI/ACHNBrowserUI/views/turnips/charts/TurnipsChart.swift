7723854d2b395de15a9e0c471ae7686c3b9a27e4
Renaud Jenny <renaud.jenny@gmail.com>
2020-05-17 15:53:21 +0100

    Refactor/turnips chart improve code maintainability and readability (#150)
    * refactor(Turnips): extract private PreferenceKey away from its struct
    
    * refactor(Turnips): use YAxis data once for all curves and values
    
    * Start with the average curve, other will be done in an other commit
    
    * refactor(Turnips): use yAxisData instead of recomputing for Bottom Legend
    
    * refactor(Turnips): Chart - use YAxis data for Chart lines drawing
    
    * refactor(Turnips): Chart - use YAxis data to draw the minBuyPrice curve
    
    * refactor(Turnips): Chart - use YAxis data for minMax curves
    
    * refactor(Turnips): Chart - use YAxis data for Values
    
    * refactor(Turnips): Chart - use YAxis data for Vertical Legend
    
    * refactor(Turnips): Chart - use YAxis data for Vertical Interactive lines
    
    * refactor(Turnips): remove useless usage of a Geometry Reader in Chart Values
    
    * refactor(Turnips): remove old and unused minMax ratios
    
    * fix(Turnips): when minimum value is greater than 50, 50 is the lowest level displayed

--- ACHNBrowserUI/ACHNBrowserUI/views/turnips/charts/TurnipsChart.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/turnips/charts/TurnipsChart.swift
@@ -11,21 +11,69 @@ import Backend
 
 enum TurnipsChart {
     static let steps = 50
-    static let extraMinSteps = 50
+    static let extraMinSteps = 0
     static let extraMaxSteps = 50
+}
 
-    enum PredictionCurve: String {
-        case minBuyPrice
-        case average
-        case minMax
+extension TurnipsChart {
+    struct YAxis {
+        var min: (value: Int, position: CGPoint)
+        var max: (value: Int, position: CGPoint)
+        var average: (value: Int, position: CGPoint)
+        var minBuyPrice: (value: Int, position: CGPoint)
+    }
 
-        var color: Color {
-            switch self {
-            case .minBuyPrice: return .graphMinimum
-            case .average: return .graphAverage
-            case .minMax: return .graphMinMax
-            }
+    static func data(for predictions: TurnipPredictions, size: CGSize) -> [YAxis] {
+        guard
+            let minMax = predictions.minMax,
+            let averages = predictions.averagePrices,
+            let minimumBuyPrice = predictions.minBuyPrice
+            else {
+                return []
         }
+
+        let minimums = minMax.compactMap { $0.first }
+        let maximums = minMax.compactMap { $0.second }
+
+        let min = minimums.min() ?? 0
+        let max = maximums.max() ?? 0
+
+        let count = minMax.count
+
+        let (ratioX, ratioY, _, maxY) = ratios(size: size, count: count, min: min, max: max)
+
+        return Array(0..<count)
+            .map({ index in
+                let x = CGFloat(index) * ratioX
+                let minPosition = CGPoint(x: x, y: (maxY - CGFloat(minimums[index])) * ratioY)
+                let maxPosition = CGPoint(x: x, y: (maxY - CGFloat(maximums[index])) * ratioY)
+                let averagePosition = CGPoint(x: x, y: (maxY - CGFloat(averages[index])) * ratioY)
+                let minimumBuyPricePosition = CGPoint(x: x, y: (maxY - CGFloat(minimumBuyPrice)) * ratioY)
+
+                return YAxis(
+                    min: (value: minimums[index], position: minPosition),
+                    max: (value: maximums[index], position: maxPosition),
+                    average: (value: averages[index], position: averagePosition),
+                    minBuyPrice: (value: minimumBuyPrice, position: minimumBuyPricePosition)
+                )
+            })
+    }
+}
+
+extension TurnipsChart {
+    typealias Ratios = (ratioX: CGFloat, ratioY: CGFloat, minY: CGFloat, maxY: CGFloat)
+
+    static func ratios(size: CGSize, count: Int, min: Int, max: Int) -> Ratios {
+        let ratioX = size.width/(CGFloat(count - 1))
+
+        let steps = TurnipsChart.steps
+
+        let computedRoundedMin = Int(min)/steps * steps - TurnipsChart.extraMinSteps
+        let roundedMin = Swift.max(0, computedRoundedMin)
+        let roundedMax = Int(max)/steps * steps + TurnipsChart.extraMaxSteps
+        let ratioY = size.height/CGFloat(roundedMax - roundedMin)
+
+        return (ratioX, ratioY, CGFloat(roundedMin), CGFloat(roundedMax))
     }
 }
 
@@ -35,41 +83,11 @@ extension Array {
     }
 }
 
-extension Array where Element == [Int] {
-    typealias MinMaxRatios = (min: CGFloat, max: CGFloat, ratioY: CGFloat, ratioX: CGFloat)
+extension Array where Element == TurnipsChart.YAxis {
+    func ratios(in rect: CGRect) -> TurnipsChart.Ratios {
+        let min = self.compactMap { $0.min.value }.min() ?? 0
+        let max = self.compactMap { $0.max.value }.max() ?? 0
 
-    private func minMaxAndRatios(
-        rect: CGRect
-    ) -> (min: CGFloat, max: CGFloat, ratioY: CGFloat, ratioX: CGFloat) {
-        let ratioX = rect.maxX/(CGFloat(self.count - 1))
-
-        let min: CGFloat = self
-            .compactMap { $0.first }
-            .map(CGFloat.init)
-            .min() ?? 0
-        let max: CGFloat = self
-            .compactMap { $0.second }
-            .map(CGFloat.init)
-            .max() ?? 0
-        let ratioY = rect.maxY/(max - min)
-
-        return (min, max, ratioY, ratioX)
-    }
-
-    func roundedMinMaxAndRatios(
-        rect: CGRect
-    ) -> (minY: CGFloat, max: CGFloat, ratioY: CGFloat, ratioX: CGFloat) {
-        let (minY, maxY, _, ratioX) = minMaxAndRatios(rect: rect)
-
-        let steps = TurnipsChart.steps
-        let extraMinSteps = TurnipsChart.extraMinSteps
-        let extraMaxSteps = TurnipsChart.extraMaxSteps
-
-        let computedRoundedMin = Int(minY)/steps * steps - extraMinSteps
-        let roundedMin = Swift.max(0, computedRoundedMin)
-        let roundedMax = (Int(maxY) + steps)/steps * steps + extraMaxSteps
-        let ratioY = rect.maxY/CGFloat(roundedMax - roundedMin)
-
-        return (CGFloat(roundedMin), CGFloat(roundedMax), ratioY, ratioX)
+        return TurnipsChart.ratios(size: rect.size, count: count, min: min, max: max)
     }
 }
