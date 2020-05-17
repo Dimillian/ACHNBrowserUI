22159acffeec5a7fccc6bd90c6de227f120ea291
Renaud Jenny <renaud.jenny@gmail.com>
2020-05-17 15:53:21 +0100

    Refactor/turnips chart improve code maintainability and readability (#150)
    * refactor(Turnips): extract private PreferenceKey away from its struct
    
    * refactor(Turnips): use YAxis data once for all curves and values
    
    * Start with the average curve, other will be done in an other commit
    
    * refactor(Turnips): use yAxisData instead of recomputing for Bottom Legend
    
    * refactor(Turnips): Chart - use YAxis data for Chart lines drawing
    
    * refactor(Turnips): Chart - use YAxis data to draw the minBuyPrice curve
    
    * refactor(Turnips): Chart - use YAxis data for minMax curves
    
    * refactor(Turnips): Chart - use YAxis data for Values
    
    * refactor(Turnips): Chart - use YAxis data for Vertical Legend
    
    * refactor(Turnips): Chart - use YAxis data for Vertical Interactive lines
    
    * refactor(Turnips): remove useless usage of a Geometry Reader in Chart Values
    
    * refactor(Turnips): remove old and unused minMax ratios
    
    * fix(Turnips): when minimum value is greater than 50, 50 is the lowest level displayed

--- ACHNBrowserUI/ACHNBrowserUI/views/turnips/charts/TurnipsChartValuesView.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/turnips/charts/TurnipsChartValuesView.swift
@@ -10,54 +10,29 @@ import SwiftUI
 import Backend
 
 struct TurnipsChartValuesView: View {
-    let predictions: TurnipPredictions
+    let data: [TurnipsChart.YAxis]
     let position: Int
     let padding: CGFloat = 16
 
     var body: some View {
-        GeometryReader(content: values)
-    }
-
-    func values(geometry: GeometryProxy) -> some View {
         ZStack {
-            Text("\(min)")
+            Text("\(data[position].min.value)")
                 .bold()
                 .foregroundColor(.graphMinMax)
                 .modifier(ValueModifier(backgroundColor: Color.acBackground))
-                .position(positions(geometry: geometry).min)
-            Text("\(average)")
+                .position(data[position].min.position)
+            Text("\(data[position].average.value)")
                 .bold()
                 .foregroundColor(.graphAverage)
                 .saturation(5)
                 .modifier(ValueModifier(backgroundColor: Color.acHeaderBackground))
-                .position(positions(geometry: geometry).average)
-            Text("\(max)")
+                .position(data[position].average.position)
+            Text("\(data[position].max.value)")
                 .bold()
                 .foregroundColor(.graphMinMax)
                 .modifier(ValueModifier(backgroundColor: Color.acBackground))
-                .position(positions(geometry: geometry).max)
+                .position(data[position].max.position)
         }
-    }
-
-    var min: Int {
-        predictions.minMax?.compactMap { $0.first }[position] ?? 0
-    }
-
-    var max: Int {
-        predictions.minMax?.compactMap { $0.second }[position] ?? 0
-    }
-
-    var average: Int { predictions.averagePrices?[position] ?? 0 }
-
-    typealias Positions = (min: CGPoint, max: CGPoint, average: CGPoint)
-    func positions(geometry: GeometryProxy) -> Positions {
-        let (_, maxY, ratioY, ratioX) = predictions.minMax?.roundedMinMaxAndRatios(rect: geometry.frame(in: .local)) ?? (0, 0, 0, 0)
-        let x = CGFloat(position) * ratioX
-        return (
-            min: CGPoint(x: x, y: ratioY * (maxY - CGFloat(min)) + padding),
-            max: CGPoint(x: x, y: ratioY * (maxY - CGFloat(max)) - padding),
-            average: CGPoint(x: x, y: ratioY * (maxY - CGFloat(average)) - padding)
-        )
     }
 }
 
@@ -82,10 +57,12 @@ struct TurnipsChartValuesView_Previews: PreviewProvider {
         @State private var position = 5.0
         var body: some View {
             VStack {
-                TurnipsChartValuesView(
-                    predictions: TurnipsChartView_Previews.predictions,
-                    position: Int(position)
-                )
+                GeometryReader {
+                    TurnipsChartValuesView(
+                        data: TurnipsChartView_Previews.yAxisData(for: $0),
+                        position: Int(self.position)
+                    )
+                }
                 Slider(value: $position, in: 0...11)
             }
         }
