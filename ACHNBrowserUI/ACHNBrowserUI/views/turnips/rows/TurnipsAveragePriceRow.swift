f7485a785875d479a3e291067cc467f172a4faf4
Renaud Jenny <renaud.jenny@gmail.com>
2020-05-23 08:39:42 +0100

    Feat: #171 show entered turnips price values instead of min max and average (#175)
    * feat(Turnips): use entered prices instead of min-max or average ones
    
    * Fix #171
    
    * fix(Turnips): select another tab change the values between average and min max
    
    * fix(Turnips): remove the visual glitch while switching from tab to tab
    
    * feat(Turnips): use acText color for entered price values

--- ACHNBrowserUI/ACHNBrowserUI/views/turnips/rows/TurnipsAveragePriceRow.swift
+++ ACHNBrowserUI/ACHNBrowserUI/views/turnips/rows/TurnipsAveragePriceRow.swift
@@ -1,5 +1,5 @@
 //
-//  TurnipsAveragePriceROw.swift
+//  TurnipsAveragePriceRow.swift
 //  ACHNBrowserUI
 //
 //  Created by Thomas Ricouard on 29/04/2020.
@@ -10,62 +10,40 @@ import SwiftUI
 
 struct TurnipsAveragePriceRow: View {
     let label: String
-    let prices: [Int]?
-    let minMaxPrices: [[Int]]?
-    
-    init(label: String, prices: [Int]) {
-        self.label = label
-        self.prices = prices
-        self.minMaxPrices = nil
-    }
-    
-    init(label: String, minMaxPrices: [[Int]]) {
-        self.label = label
-        self.prices = nil
-        self.minMaxPrices = minMaxPrices
-    }
-    
-    private func color(price: Int) -> Color {
-        if price <= 90 {
-            return .red
-        } else if price >= 150 {
-            return .acTabBarBackground
-        } else {
-            return .acSecondaryText
-        }
-    }
-    
-    private func color(prices: [Int]) -> Color {
-        color(price: Int(prices.average))
-    }
-    
+    let prices: [Int]
+    let minMaxPrices: [[Int]]
+
     var body: some View {
         HStack {
             Text(LocalizedStringKey(label))
                 .font(.body)
                 .foregroundColor(.acText)
             Spacer()
-            if prices != nil {
-                Text("\(prices!.first!)")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(color(price: prices!.first!))
-                Spacer()
-                Text("\(prices!.last!)")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(color(price: prices!.last!))
-            } else if minMaxPrices != nil {
-                Text("\(minMaxPrices!.first!.first!) - \(minMaxPrices!.first!.last!)")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(color(prices: minMaxPrices!.first!))
-                Spacer()
-                Text("\(minMaxPrices!.last!.first!) - \(minMaxPrices!.last!.last!)")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundColor(color(prices: minMaxPrices!.last!))
-            }
+            Text("\(prices.first!)")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(color(meridian: .am))
+            Spacer()
+            Text("\(prices.last!)")
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(color(meridian: .pm))
+        }
+    }
+}
+
+extension TurnipsAveragePriceRow: TurnipsPriceRow {
+    enum Meridian { case am, pm }
+
+    var averagePrices: [Int] { prices }
+
+    func color(meridian: Meridian) -> Color {
+        if isEntered(meridian: meridian) {
+            return .acText
+        } else if let price = meridian == .am ? prices.first : prices.last {
+            return color(price: price)
+        } else {
+            return .clear
         }
     }
 }
@@ -74,8 +52,8 @@ struct TurnipsAveragePriceRow_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             List {
-                TurnipsAveragePriceRow(label: "Mon", prices: [90, 95])
-                TurnipsAveragePriceRow(label: "Mon", prices: [40, 600])
+                TurnipsAveragePriceRow(label: "Mon", prices: [90, 95], minMaxPrices: [[90, 90], [81, 141]])
+                TurnipsAveragePriceRow(label: "Mon", prices: [40, 600], minMaxPrices: [[40, 81], [250, 600]])
             }
         }
     }
