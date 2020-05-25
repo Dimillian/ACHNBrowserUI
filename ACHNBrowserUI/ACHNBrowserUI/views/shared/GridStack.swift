c05456ff74599e55b9296a2b54daa5151bab6bef
Renaud Jenny <renaud.jenny@gmail.com>
2020-05-25 10:10:36 +0100

    Feat/turnips improve grid view alignments (#191)
    * feat(Turnips): improve Grid view alignments for values
    
    * Add new GridStack to deal with values like this instead of List rows
    
    * refactor(Turnips): remove useless Grid row
    
    * We use Grid View values directly in TurnipsView now

---
+++ ACHNBrowserUI/ACHNBrowserUI/views/shared/GridStack.swift
@@ -0,0 +1,53 @@
+//
+//  GridStack.swift
+//  ACHNBrowserUI
+//
+//  Created by Renaud JENNY on 24/05/2020.
+//  Copyright © 2020 Thomas Ricouard. All rights reserved.
+//
+
+import SwiftUI
+
+// See https://www.hackingwithswift.com/quick-start/swiftui/how-to-position-views-in-a-grid
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let spacing: CGFloat?
+    let content: (Int, Int) -> Content
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: spacing) {
+            ForEach(0..<rows, id: \.self) { row in
+                VStack {
+                    if row == 0 { Spacer() }
+                    HStack {
+                        ForEach(0..<self.columns, id: \.self) { column in
+                            HStack {
+                                column > 0 ? Spacer().eraseToAnyView() : EmptyView().eraseToAnyView()
+                                self.content(row, column)
+                                Spacer()
+                            }
+                        }
+                    }
+                    if row == self.rows - 1 {
+                        Spacer()
+                    } else {
+                        Divider()
+                    }
+                }
+            }
+        }
+    }
+
+    init(
+        rows: Int,
+        columns: Int,
+        spacing: CGFloat? = nil,
+        @ViewBuilder content: @escaping (Int, Int) -> Content
+    ) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+        self.spacing = spacing
+    }
+}
