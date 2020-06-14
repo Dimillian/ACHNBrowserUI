3059eb46e24aa9d2c34ff52c23ba2435ca1aa507
Rohan Ramakrishnan <rohanrk2009@gmail.com>
2020-06-14 16:28:54 -0700

    Create new classes to manage notifications

---
+++ ACHNBrowserUI/ACHNBrowserUI/packages/Backend/Sources/Backend/services/SettingsService.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rohan Ramakrishnan on 6/14/20.
+//
+
+import Foundation
---
+++ ACHNBrowserUI/ACHNBrowserUI/viewModels/SettingsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewModel.swift
+//  ACHNBrowserUI
+//
+//  Created by Rohan Ramakrishnan on 6/14/20.
+//  Copyright © 2020 Thomas Ricouard. All rights reserved.
+//
+
+import Foundation
